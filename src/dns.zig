de1914f4c8d963164151b50231233433cc8ad955
Adriano Anastácio <adriano.halo@gmail.com>
2024-12-17 23:03:50 +0000

    mostly complete getaddressinfo

--- src/dns.zig
+++ src/dns.zig
@@ -1,5 +1,39 @@
 const std = @import("std");
-// todo: actual dns
+const log = std.log;
+
+const c = @cImport({
+    @cInclude("netdb.h");
+    @cInclude("sys/socket.h");
+    @cInclude("arpa/inet.h");
+});
+
+pub fn checkIp(endpoint: []const u8, ip: []const u8) !bool {
+    var addr_info: [*c]c.addrinfo = undefined;
+    const size: isize = c.getaddrinfo(endpoint.ptr, null, null, &addr_info);
+    if (size != 0) {
+        log.err("getaddrinfo responded with {d}", .{size});
+        return error.CouldNotCheckIp;
+    }
+    defer c.freeaddrinfo(addr_info);
+
+    var curr = addr_info;
+    while (true) {
+        if (addr_info.*.ai_family == c.AF_INET) {
+            const socket_addr: [*c]c.sockaddr_in = @ptrCast(@alignCast(addr_info.*.ai_addr));
+            const c_addr: [*c]u8 = c.inet_ntoa(socket_addr.*.sin_addr);
+            const addr = std.mem.span(c_addr);
+
+            log.info("comparing {s} == {s}", .{ ip, addr });
+
+            if (std.mem.eql(u8, addr, ip)) {
+                return true;
+            }
+        }
+        curr = addr_info.*.ai_next orelse return false;
+    }
+}
+
+// todo: actual dns implementation
 pub const Header = packed struct {
     id: u16,
     qr: u1,
--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const http = std.http;
 const time = std.time;
 const log = std.log;
+const dns = @import("dns.zig");
 
 const Allocator = std.mem.Allocator;
 const Client = std.http.Client;
@@ -24,11 +25,32 @@ pub fn main() !void {
     var client = Client{ .allocator = allocator };
     defer client.deinit();
 
-    const public_ip: []const u8 = try get_public_ip(&client, params.get_public_ip_url);
-    log.info("got public ip: {s} from {s}", .{ public_ip, params.get_public_ip_url });
+    var pub_ip_buf: [12]u8 = undefined;
+    const size = try get_public_ip(&client, params.get_public_ip_url, &pub_ip_buf);
+    const public_ip: []const u8 = pub_ip_buf[0..size];
+
+    log.info("got public ip: \"{s}\" from {s}", .{ public_ip, params.get_public_ip_url });
     while (true) {
         for (params.list.items) |param| {
             log.info("host: {s} - domain: {s}", .{ param.host, param.domain });
+
+            const parts = switch (param.host[0]) {
+                '@', '.' => ([_][]const u8{param.domain})[0..],
+                else => ([_][]const u8{ param.host, ".", param.domain })[0..],
+            };
+
+            const endpoint = try std.mem.join(allocator, "", parts);
+            {
+                defer allocator.free(endpoint);
+                const is_same_ip = try dns.checkIp(endpoint, public_ip);
+
+                if (!is_same_ip) {
+                    log.info("updating IP to {s}", .{public_ip});
+                    try update_ip(allocator, public_ip);
+                } else {
+                    log.info("IP already up to date", .{});
+                }
+            }
         }
         log.info("iteration finished - going to sleep", .{});
         time.sleep(sleep_time);
@@ -37,7 +59,9 @@ pub fn main() !void {
     // foreach param make a request to check if ip is the same
 }
 
-pub fn get_public_ip(client: *Client, url: []const u8) ![]const u8 {
+pub fn update_ip(_: Allocator, _: []const u8) !void {}
+
+pub fn get_public_ip(client: *Client, url: []const u8, buffer: []u8) !u64 {
     const uri = try std.Uri.parse(url);
 
     var header_buf: [1024]u8 = undefined;
@@ -48,14 +72,8 @@ pub fn get_public_ip(client: *Client, url: []const u8) ![]const u8 {
     defer request.deinit();
 
     try request.send();
+
     try request.wait();
 
-    var buffer: [11]u8 = undefined;
-    var response = request.response;
-
-    if (response.content_length) |len| {
-        try response.parse(&buffer); // todo
-        return buffer[0..len];
-    }
-    return error.ResponseWithNoContent;
+    return try request.readAll(buffer);
 }
