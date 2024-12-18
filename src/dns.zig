32bdbc3c8d15836a0dfb3df36b1c4a91d083cbd8
Adriano Anastácio <adriano.halo@gmail.com>
2024-12-18 23:50:16 +0000

    fixed getaddrinfo

--- src/dns.zig
+++ src/dns.zig
@@ -4,22 +4,41 @@ const log = std.log;
 const c = @cImport({
     @cInclude("netdb.h");
     @cInclude("sys/socket.h");
+    @cInclude("sys/types.h");
     @cInclude("arpa/inet.h");
 });
 
+pub const CheckIpError = error{ EndpointDoesntFitInBuffer, GetAddressInfoFailed };
+
 pub fn checkIp(endpoint: []const u8, ip: []const u8) !bool {
     var addr_info: [*c]c.addrinfo = undefined;
-    const size: isize = c.getaddrinfo(endpoint.ptr, null, null, &addr_info);
-    if (size != 0) {
-        log.err("getaddrinfo responded with {d}", .{size});
-        return error.CouldNotCheckIp;
+
+    var hints: c.addrinfo = c.addrinfo{};
+
+    const buf_len: comptime_int = 128;
+
+    // deal with possible buff overflow, for now err
+    if (endpoint.len >= buf_len) {
+        log.err("endpoint does not fit in buffer: ", .{endpoint});
+        return CheckIpError.EndpointDoesntFitInBuffer;
+    }
+
+    var endpoint_buf: [buf_len]u8 = undefined;
+    const endpoint_ptr: [*c]const u8 = try std.fmt.bufPrintZ(&endpoint_buf, "{s}", .{endpoint});
+    log.info("ENDPOINT = \"{s}\"", .{endpoint_ptr});
+
+    const code: c_int = c.getaddrinfo(endpoint_ptr, null, &hints, &addr_info);
+    if (code != 0) {
+        const err = c.gai_strerror(code);
+        log.err("getaddrinfo responded with {d} - {s}", .{ code, err });
+        return CheckIpError.GetAddressInfoFailed;
     }
     defer c.freeaddrinfo(addr_info);
 
     var curr = addr_info;
     while (true) {
-        if (addr_info.*.ai_family == c.AF_INET) {
-            const socket_addr: [*c]c.sockaddr_in = @ptrCast(@alignCast(addr_info.*.ai_addr));
+        if (curr.*.ai_family == c.AF_INET) {
+            const socket_addr: [*c]c.sockaddr_in = @ptrCast(@alignCast(curr.*.ai_addr));
             const c_addr: [*c]u8 = c.inet_ntoa(socket_addr.*.sin_addr);
             const addr = std.mem.span(c_addr);
 
@@ -29,7 +48,7 @@ pub fn checkIp(endpoint: []const u8, ip: []const u8) !bool {
                 return true;
             }
         }
-        curr = addr_info.*.ai_next orelse return false;
+        curr = curr.*.ai_next orelse return false;
     }
 }
 
